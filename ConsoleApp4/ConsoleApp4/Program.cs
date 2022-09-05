07fa1f31762cd5d98e1c1977eed2dc9fd5da472d
Huyett <Todd.Huyett@estes-express.com>
2022-09-04 22:43:46 -0400

    'd'

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows.Automation;
+using System.Diagnostics;
+using System.Threading;
+namespace TestScenario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Process p = Process.Start("C:\\Windows\\notepad.exe");
+                Thread.Sleep(5000);
+                AutomationElement aeDesktop = AutomationElement.RootElement;
+                AutomationElement aeForm = null;
+                int numWaits = 0;
+                do
+                {
+                    Console.WriteLine("Looking for Notepad");
+                    aeForm = aeDesktop.FindFirst(TreeScope.Children,
+                      new PropertyCondition(AutomationElement.ClassNameProperty, "Notepad"));
+                    ++numWaits;
+                    Thread.Sleep(100);
+                } while (aeForm == null && numWaits < 50);
+                if (aeForm == null) Console.WriteLine("Did not find it");
+                else Console.WriteLine("Found it!");
+                Thread.Sleep(5000);
+
+                AutomationElement menuTab = aeForm.FindFirst(TreeScope.Children,
+                new PropertyCondition(AutomationElement.AutomationIdProperty, "MenuBar"));
+
+                AutomationElement file = menuTab.FindFirst(TreeScope.Children,
+                new PropertyCondition(AutomationElement.NameProperty, "File"));
+                
+                Thread.Sleep(5000);
+                Console.WriteLine("Selecting File");
+                ExpandCollapsePattern ipClickButton1 = (ExpandCollapsePattern)file.GetCurrentPattern(ExpandCollapsePattern.Pattern);
+                ipClickButton1.Expand();
+             
+                Thread.Sleep(5000);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Fatal error: " + ex.Message);
+            }
+        }
+    }
+}
