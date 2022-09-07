afbdaf31b143f6c70df7b691af84df7545d7c7ef
Huyett <Todd.Huyett@estes-express.com>
2022-09-07 12:10:52 -0400

    'd'

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -2,10 +2,13 @@
 using System.Windows.Automation;
 using System.Diagnostics;
 using System.Threading;
+using System.Xml.Linq;
+using System.Windows.Forms;
 namespace TestScenario
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             try
@@ -27,18 +30,23 @@ namespace TestScenario
                 else Console.WriteLine("Found it!");
                 Thread.Sleep(5000);
 
-                AutomationElement menuTab = aeForm.FindFirst(TreeScope.Children,
-                new PropertyCondition(AutomationElement.AutomationIdProperty, "MenuBar"));
-                Thread.Sleep(5000);
-                Console.WriteLine(menuTab.ToString());
-                Thread.Sleep(5000);
-                AutomationElement file = menuTab.FindFirst(TreeScope.Children,
-                new PropertyCondition(AutomationElement.NameProperty, "File"));
 
-                Thread.Sleep(5000);
-                Console.WriteLine("Selecting File");
-                ExpandCollapsePattern ipClickButton1 = (ExpandCollapsePattern)file.GetCurrentPattern(ExpandCollapsePattern.Pattern);
-                ipClickButton1.Expand();
+
+
+  
+                AutomationElement editBox = aeForm.FindFirst(TreeScope.Children, new PropertyCondition(AutomationElement.NameProperty, "Edit"));
+              
+                Thread.Sleep(2000);
+
+            
+                SendKeys.SendWait("Hello World");
+
+
+
+
+
+
+
 
                 Thread.Sleep(5000);
 
@@ -48,5 +56,67 @@ namespace TestScenario
                 Console.WriteLine("Fatal error: " + ex.Message);
             }
         }
+        private static void InsertText(AutomationElement targetControl,
+                                    string value)
+        {
+            // Validate arguments / initial setup
+            if (value == null)
+                throw new ArgumentNullException(
+                    "String parameter must not be null.");
+
+            if (targetControl == null)
+                throw new ArgumentNullException(
+                    "AutomationElement parameter must not be null");
+
+            // A series of basic checks prior to attempting an insertion.
+            //
+            // Check #1: Is control enabled?
+            // An alternative to testing for static or read-only controls 
+            // is to filter using 
+            // PropertyCondition(AutomationElement.IsEnabledProperty, true) 
+            // and exclude all read-only text controls from the collection.
+            if (!targetControl.Current.IsEnabled)
+            {
+                throw new InvalidOperationException(
+                    "The control is not enabled.\n\n");
+            }
+
+            // Check #2: Are there styles that prohibit us 
+            //           from sending text to this control?
+            if (!targetControl.Current.IsKeyboardFocusable)
+            {
+                throw new InvalidOperationException(
+                    "The control is not focusable.\n\n");
+            }
+
+            // Once you have an instance of an AutomationElement,  
+            // check if it supports the ValuePattern pattern.
+            object valuePattern = null;
+
+            if (!targetControl.TryGetCurrentPattern(
+                ValuePattern.Pattern, out valuePattern))
+            {
+                // Elements that support TextPattern 
+                // do not support ValuePattern and TextPattern
+                // does not support setting the text of 
+                // multi-line edit or document controls.
+                // For this reason, text input must be simulated.
+            }
+            // Control supports the ValuePattern pattern so we can 
+            // use the SetValue method to insert content.
+            else
+            {
+                if (((ValuePattern)valuePattern).Current.IsReadOnly)
+                {
+                    throw new InvalidOperationException(
+                        "The control is read-only.");
+                }
+                else
+                {
+                    ((ValuePattern)valuePattern).SetValue(value);
+                }
+            }
+        }
     }
+
 }
