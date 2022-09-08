5b039bfdf7dfdc759938301bc8d485b622390e08
Huyett <Todd.Huyett@estes-express.com>
2022-09-07 22:56:06 -0400

    'd'

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -1,141 +1,49 @@
 ﻿using System;
-using System.Windows.Automation;
-using System.Diagnostics;
-using System.Threading;
-using System.Xml.Linq;
 using System.Windows.Forms;
-namespace TestScenario
+using Interop.UIAutomationClient;
+using System.Threading;
+
+
+
+
+
+namespace PrintDesktopUiaElementNameViaCom
 {
-    class Program
-    {
+
+    class PrintDesktopUiaElementNameViaComProgram    {
 
         static void Main(string[] args)
         {
-            try
-            {
-                Process p = Process.Start("C:\\Windows\\notepad.exe");
-                Thread.Sleep(5000);
-                AutomationElement aeDesktop = AutomationElement.RootElement;
-                AutomationElement aeForm = null;
-                int numWaits = 0;
-                do
-                {
-                    Console.WriteLine("Looking for Notepad");
-                    aeForm = aeDesktop.FindFirst(TreeScope.Children,
-                      new PropertyCondition(AutomationElement.ClassNameProperty, "Notepad"));
-                    ++numWaits;
-                    Thread.Sleep(100);
-                } while (aeForm == null && numWaits < 50);
-                if (aeForm == null) Console.WriteLine("Did not find it");
-                else Console.WriteLine("Found it!");
-                Thread.Sleep(5000);
+            string testAppName = "Text Editor";
+            int propertyIdName = 30005;
+            string className = "Notepad";
+            int propertyIdClassName = 30012;
+            IUIAutomation uiautomation = new CUIAutomation();
+            IUIAutomationElement rootElement = uiautomation.GetRootElement();
+            Thread.Sleep(2000);
+            IUIAutomationCondition notepadName =
+ uiautomation.CreatePropertyCondition(
+     propertyIdClassName, className);
+            Thread.Sleep(2000);
+            IUIAutomationElement mainDialog =
+                rootElement.FindFirst(
+                    TreeScope.TreeScope_Children,
+                    notepadName);
+            Thread.Sleep(2000);
+            IUIAutomationCondition conditionTestAppName =
+    uiautomation.CreatePropertyCondition(
+        propertyIdName, testAppName);
+            Thread.Sleep(2000);
+            IUIAutomationElement esditor =
+                mainDialog.FindFirst(
+                    TreeScope.TreeScope_Children,
+                    conditionTestAppName);
+            Thread.Sleep(2000);
+            SendKeys.SendWait("Here is some text");
+            Thread.Sleep(2000);
 
-                AutomationElement menuTab = null;
-                int numWaits2 = 0;
-                do
-                {
-                    Console.WriteLine("Looking for Menu");
-                    menuTab = aeForm.FindFirst(TreeScope.Children,
-                       new PropertyCondition(AutomationElement.NameProperty, "Application"));
-                    ++numWaits2;
-                    Thread.Sleep(100);
-                } while (menuTab == null && numWaits2 < 50);
-                if (menuTab == null) Console.WriteLine("Did not find it");
-                else Console.WriteLine("Found it!");
-                Thread.Sleep(5000);
-
-                AutomationElement file = null;
-                int numWaits3 = 0;
-                do
-                {
-                    Console.WriteLine("Looking for File");
-                    file = menuTab.FindFirst(TreeScope.Children,
-                       new PropertyCondition(AutomationElement.NameProperty, "File"));
-                    ++numWaits2;
-                    Thread.Sleep(100);
-                } while (file == null && numWaits3 < 50);
-                if (file == null) Console.WriteLine("Did not find it");
-                else Console.WriteLine("Found it!");
-                Thread.Sleep(5000);
-
-
-
-
-                Thread.Sleep(5000);
-                Console.WriteLine("Selecting File");
-                ExpandCollapsePattern ipClickButton1 = (ExpandCollapsePattern)file.GetCurrentPattern(ExpandCollapsePattern.Pattern);
-                Thread.Sleep(5000);
-                ipClickButton1.Expand();
-
-                Thread.Sleep(5000);
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Fatal error: " + ex.Message);
-            }
         }
-        private static void InsertText(AutomationElement targetControl,
-                                    string value)
-        {
-            // Validate arguments / initial setup
-            if (value == null)
-                throw new ArgumentNullException(
-                    "String parameter must not be null.");
-
-            if (targetControl == null)
-                throw new ArgumentNullException(
-                    "AutomationElement parameter must not be null");
-
-            // A series of basic checks prior to attempting an insertion.
-            //
-            // Check #1: Is control enabled?
-            // An alternative to testing for static or read-only controls 
-            // is to filter using 
-            // PropertyCondition(AutomationElement.IsEnabledProperty, true) 
-            // and exclude all read-only text controls from the collection.
-            if (!targetControl.Current.IsEnabled)
-            {
-                throw new InvalidOperationException(
-                    "The control is not enabled.\n\n");
-            }
-
-            // Check #2: Are there styles that prohibit us 
-            //           from sending text to this control?
-            if (!targetControl.Current.IsKeyboardFocusable)
-            {
-                throw new InvalidOperationException(
-                    "The control is not focusable.\n\n");
-            }
-
-            // Once you have an instance of an AutomationElement,  
-            // check if it supports the ValuePattern pattern.
-            object valuePattern = null;
-
-            if (!targetControl.TryGetCurrentPattern(
-                ValuePattern.Pattern, out valuePattern))
-            {
-                // Elements that support TextPattern 
-                // do not support ValuePattern and TextPattern
-                // does not support setting the text of 
-                // multi-line edit or document controls.
-                // For this reason, text input must be simulated.
-            }
-            // Control supports the ValuePattern pattern so we can 
-            // use the SetValue method to insert content.
-            else
-            {
-                if (((ValuePattern)valuePattern).Current.IsReadOnly)
-                {
-                    throw new InvalidOperationException(
-                        "The control is read-only.");
-                }
-                else
-                {
-                    ((ValuePattern)valuePattern).SetValue(value);
-                }
-            }
-        }
-    }
-
+    } 
+        
+    
 }
