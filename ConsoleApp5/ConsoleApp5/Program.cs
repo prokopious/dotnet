ce4cbcb4d9f0f3a68892a55f9bcf2369f768b237
Huyett <Todd.Huyett@estes-express.com>
2022-09-09 08:39:46 -0400

    'd'

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Automation;
+using System.Diagnostics;
+using System.Threading;
+namespace TestScenario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Process p = Process.Start("C:\\Windows\\notepad.exe");
+                Thread.Sleep(5000);
+                AutomationElement aeDesktop = AutomationElement.RootElement;
+                AutomationElement aeForm = null;
+                int numWaits = 0;
+                do
+                {
+                    Console.WriteLine("Looking for Notepad");
+                    aeForm = aeDesktop.FindFirst(TreeScope.Children,
+                      new PropertyCondition(AutomationElement.ClassNameProperty, "Notepad"));
+                    ++numWaits;
+                    Thread.Sleep(100);
+                } while (aeForm == null && numWaits < 50);
+                if (aeForm == null) Console.WriteLine("Did not find it");
+                else Console.WriteLine("Found it!");
+                Thread.Sleep(5000);
+
+                AutomationElement textField = aeForm.FindFirst(TreeScope.Children,
+                new PropertyCondition(AutomationElement.AutomationIdProperty, "15"));
+                Console.WriteLine(textField.Current.Name);
+                Thread.Sleep(5000);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Fatal error: " + ex.Message);
+            }
+        }
+    }
+}
