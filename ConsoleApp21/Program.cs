204f95d3c678f72577e4b8f569f38f35eed1d0f4
Huyett <Todd.Huyett@estes-express.com>
2022-09-18 11:33:42 -0400

    'd'

--- ConsoleApp21/Program.cs
+++ ConsoleApp21/Program.cs
@@ -30,8 +30,10 @@ namespace TestScenario
             IUIAutomationElement continueButton = dialog.FindFirst(TreeScope.TreeScope_Children, condition2);
 
             Console.WriteLine(continueButton.CurrentName);
- 
+            Thread.Sleep(2000);
             IUIAutomationInvokePattern invokePatternFileTab = (IUIAutomationInvokePattern)continueButton.GetCurrentPattern(10000);
+            Console.WriteLine(invokePatternFileTab.ToString());
+            Thread.Sleep(2000);
             invokePatternFileTab.Invoke();
 
             Thread.Sleep(10000);
