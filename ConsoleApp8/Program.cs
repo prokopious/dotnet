e09901bf7f48ce1f2a92fded1b9930da82bbfe16
Huyett <Todd.Huyett@estes-express.com>
2022-09-18 19:03:16 -0400

    'd'

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -8,45 +8,33 @@ namespace TestScenario
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Process p = Process.Start("C:\\Windows\\notepad.exe");
-                Thread.Sleep(5000);
-                AutomationElement aeDesktop = AutomationElement.RootElement;
-                Console.WriteLine("found dessfsdfktop");
-                AutomationElement aeForm = null;
-                int numWaits = 0;
-                do
-                {
-                    Console.WriteLine("Looking for Notepad");
-                    aeForm = aeDesktop.FindFirst(TreeScope.Children,
-                      new PropertyCondition(AutomationElement.ClassNameProperty, "Notepad"));
-                    ++numWaits;
-                    Thread.Sleep(100);
-                } while (aeForm == null && numWaits < 50);
-                if (aeForm == null) Console.WriteLine("Did not find it");
-                else Console.WriteLine("Found it!");
-                Thread.Sleep(5000);
 
-                AutomationElement menuTab = aeForm.FindFirst(TreeScope.Children,
-                new PropertyCondition(AutomationElement.AutomationIdProperty, "MenuBar"));
-                Console.WriteLine(menuTab.ToString());
-                Thread.Sleep(5000);
-                AutomationElement file = menuTab.FindFirst(TreeScope.Children,
-                new PropertyCondition(AutomationElement.NameProperty, "File"));
+            //Process p = Process.Start("C:\\Windows\\notepad.exe");
+            Thread.Sleep(5000);
+            AutomationElement aeDesktop = AutomationElement.RootElement;
+            Console.WriteLine("found desktop");
 
-                Thread.Sleep(5000);
-                Console.WriteLine("Selecting File");
-                ExpandCollapsePattern ipClickButton1 = (ExpandCollapsePattern)file.GetCurrentPattern(ExpandCollapsePattern.Pattern);
-                ipClickButton1.Expand();
+   
+            AutomationElement dotnet = aeDesktop.FindFirst(TreeScope.Children,
+              new PropertyCondition(AutomationElement.NameProperty, "Microsoft .NET Framework"));
+            Console.WriteLine("found edock");
 
-                Thread.Sleep(5000);
 
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Fatal error: " + ex.Message);
-            }
+
+            AutomationElement cont = dotnet.FindFirst(TreeScope.Children,
+            new PropertyCondition(AutomationElement.NameProperty, "Continue"));
+
+            Console.WriteLine("found continue");
+
+
+
+
+
+            Thread.Sleep(5000);
+
+
+
+
         }
     }
 }
