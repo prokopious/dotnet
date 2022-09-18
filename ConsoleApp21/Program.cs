ee94045ce6ed67581e40e8556bc62d39423841dd
Huyett <Todd.Huyett@estes-express.com>
2022-09-18 10:35:19 -0400

    'd'

--- ConsoleApp21/Program.cs
+++ ConsoleApp21/Program.cs
@@ -18,14 +18,14 @@ namespace TestScenario
             // Get the root element
             IUIAutomationElement rootElement = _automation.GetRootElement();
             // Get its name
-
+            Console.WriteLine("got the root element");
             IUIAutomationCondition condition = _automation.CreatePropertyCondition(UIA_PropertyIds.UIA_NamePropertyId, "eDock.exe - .NET Framework Initialization Error");
           
             IUIAutomationElement e = rootElement.FindFirst(TreeScope.TreeScope_Children, condition);
 
             Console.WriteLine(e.CurrentClassName);
 
-            IUIAutomationCondition condition2 = _automation.CreatePropertyCondition(UIA_PropertyIds.UIA_AutomationIdPropertyId, "1");
+            IUIAutomationCondition condition2 = _automation.CreatePropertyCondition(UIA_PropertyIds.UIA_AutomationIdPropertyId, "MenuBar");
 
             IUIAutomationElement e2 = e.FindFirst(TreeScope.TreeScope_Children, condition2);
 
