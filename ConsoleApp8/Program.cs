a4dc79278f3962740207f0adb4c6f204902d5e67
Huyett <Todd.Huyett@estes-express.com>
2022-09-18 20:56:44 -0400

    'd'

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -13,22 +13,19 @@ namespace TestScenario
             Thread.Sleep(5000);
             AutomationElement aeDesktop = AutomationElement.RootElement;
             Console.WriteLine("found desktop");
-
+            Thread.Sleep(1000);
             //find login window for edock
             AutomationElement loginWindow = aeDesktop.FindFirst(TreeScope.Children,
             new PropertyCondition(AutomationElement.AutomationIdProperty, "frmLogin"));
             Console.WriteLine("found login window");
-
+            Thread.Sleep(1000);
             AutomationElement loginForm = loginWindow.FindFirst(TreeScope.Children,
             new PropertyCondition(AutomationElement.AutomationIdProperty, "pnlLogin"));
             Console.WriteLine("found login form");
-
-            Condition conditions = new AndCondition(
-                       new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Button),
-                       new PropertyCondition(AutomationElement.NameProperty, "Log In")
-                       );
+            Thread.Sleep(1000);
+      
             //find forklift id field
-            AutomationElement loginButton = loginForm.FindFirst(TreeScope.Children, conditions
+            AutomationElement loginButton = loginForm.FindFirst(TreeScope.Children, new PropertyCondition(AutomationElement.NameProperty, "Log In")
             );
             Console.WriteLine("found login button");
             Thread.Sleep(1000);
@@ -37,9 +34,9 @@ namespace TestScenario
             InvokePattern invokePattern =
                    loginButton.GetCurrentPattern(InvokePattern.Pattern)
                    as InvokePattern;
-
+            Thread.Sleep(1000);
             invokePattern.Invoke();
-
+               Thread.Sleep(1000);
             Console.WriteLine("clicked login button");
 
             Thread.Sleep(5000);
