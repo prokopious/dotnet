8459be8eb0dbc66b23ce7b25cb931ceb46feb784
Huyett <Todd.Huyett@estes-express.com>
2022-09-19 16:36:18 -0400

    'd'

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using UIAutomationClient;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IUIAutomation automation = new CUIAutomation();
+            IUIAutomationElement desktop = automation.GetRootElement();
+
+            IUIAutomationCondition processIdCondition = automation.CreatePropertyCondition(30005, "Untitled - Notepad");
+            IUIAutomationElement e = desktop.FindFirst(TreeScope.TreeScope_Children, processIdCondition);
+            Thread.Sleep(5000);
+            SendKeys.SendWait("sdfsdfsd");
+
+            Console.WriteLine(e.CurrentClassName);
+            //get the first Frame Tab element found.
+            Thread.Sleep(15000);
+
+        }
+    }
+}
